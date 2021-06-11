3d00bc230020955b7cb15cf963a1914b02291c81
Chris Donahue <christopherdonahue@gmail.com>
2021-06-10 18:22:29 -0700

    Adds jukebox

--- representations/jukebox/main.py
+++ representations/jukebox/main.py
@@ -1,6 +1,102 @@
-SAMPLE_RATE = 44100
-FRAME_HOP_LENGTH = 128
-NUM_FRAMES = 8192
+import librosa as lr
+import numpy as np
+
+JUKEBOX_SAMPLE_RATE = 44100
+T = 8192
+
+
+def load_audio_from_file(fpath):
+    audio, _ = lr.load(fpath, sr=JUKEBOX_SAMPLE_RATE)
+    if audio.ndim == 1:
+        audio = audio[np.newaxis]
+    audio = audio.mean(axis=0)
+
+    # normalize audio
+    norm_factor = np.abs(audio).max()
+    if norm_factor > 0:
+        audio /= norm_factor
+
+    return audio.flatten()
+
+
+def get_z(audio, vqvae):
+    # don't compute unnecessary discrete encodings
+    audio = audio[: JUKEBOX_SAMPLE_RATE * 25]
+
+    zs = vqvae.encode(t.cuda.FloatTensor(audio[np.newaxis, :, np.newaxis]))
+
+    z = zs[-1].flatten()[np.newaxis, :]
+
+    return z
+
+
+def get_cond(hps, top_prior):
+    sample_length_in_seconds = 62
+
+    hps.sample_length = (
+        int(sample_length_in_seconds * hps.sr) // top_prior.raw_to_tokens
+    ) * top_prior.raw_to_tokens
+
+    # NOTE: the 'lyrics' parameter is required, which is why it is included,
+    # but it doesn't actually change anything about the `x_cond`, `y_cond`,
+    # nor the `prime` variables
+    metas = [
+        dict(
+            artist="unknown",
+            genre="unknown",
+            total_length=hps.sample_length,
+            offset=0,
+            lyrics="""lyrics go here!!!""",
+        ),
+    ] * hps.n_samples
+
+    labels = [None, None, top_prior.labeller.get_batch_labels(metas, "cuda")]
+
+    x_cond, y_cond, prime = top_prior.get_cond(None, top_prior.get_y(labels[-1], 0))
+
+    x_cond = x_cond[0, :T][np.newaxis, ...]
+    y_cond = y_cond[0][np.newaxis, ...]
+
+    return x_cond, y_cond
+
+
+def get_final_activations(z, x_cond, y_cond, top_prior):
+
+    x = z[:, :T]
+
+    # make sure that we get the activations
+    top_prior.prior.only_encode = True
+
+    # encoder_kv and fp16 are set to the defaults, but explicitly so
+    out = top_prior.prior.forward(
+        x, x_cond=x_cond, y_cond=y_cond, encoder_kv=None, fp16=False
+    )
+
+    return out
+
+
+def get_acts_from_file(fpath, hps, vqvae, top_prior, meanpool=True):
+    audio = load_audio_from_file(fpath)
+
+    # run vq-vae on the audio
+    z = get_z(audio, vqvae)
+
+    # get conditioning info
+    x_cond, y_cond = get_cond(hps, top_prior)
+
+    # get the activations from the LM
+    acts = get_final_activations(z, x_cond, y_cond, top_prior)
+
+    # postprocessing
+    acts = acts.squeeze().type(t.float16)
+
+    if meanpool:
+        acts = acts.mean(dim=0)
+
+    acts = np.array(acts.cpu())
+
+    return acts
+
 
 if __name__ == "__main__":
     import pathlib
@@ -8,9 +104,9 @@ if __name__ == "__main__":
 
     # imports and set up Jukebox's multi-GPU parallelization
     import jukebox
+    import librosa
     import numpy as np
     import torch
-    import librosa
     from jukebox.hparams import Hyperparams, setup_hparams
     from jukebox.make_models import MODELS, make_prior, make_vqvae
     from jukebox.utils.dist_utils import setup_dist_from_mpi
@@ -69,27 +165,15 @@ if __name__ == "__main__":
             )
 
             # Set up language model
-            #hparams = setup_hparams(priors[-1], dict())
-            #hparams["prior_depth"] = 36
-            #top_prior = make_prior(hparams, vqvae, device)
+            # hparams = setup_hparams(priors[-1], dict())
+            # hparams["prior_depth"] = 36
+            # top_prior = make_prior(hparams, vqvae, device)
 
             loaded = True
 
         # Decode, resample, convert to mono, and normalize audio
-        audio, sr = librosa.load(input_path, sr=SAMPLE_RATE)
-        if audio.ndim == 1:
-            audio = audio[np.newaxis]
-        audio = audio.mean(axis=0)
-        norm_factor = np.abs(audio).max()
-        if norm_factor > 0:
-            audio /= norm_factor
-
-        # Crop audio to first ~24 seconds
-        audio = audio[:FRAME_HOP_LENGTH*NUM_FRAMES]
-
         with torch.no_grad():
-            # Codify
-            audio = torch.tensor(audio[np.newaxis, :, np.newaxis], dtype=torch.float32, device=device)
-            audio_codified = vqvae.encode(audio)[-1].unsqueeze(dim=0)
+            acts = get_acts_from_file(input_path, hps, vqvae, top_prior, meanpool=True)
+            acts = acts.cpu().numpy()
 
-        np.save(output_path, representation)
+        np.save(output_path, acts)
